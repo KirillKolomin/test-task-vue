920d1de97ad828eb0f1e74f3f94a0e12ece6d813
kirill kolomin <kirillvintorez@gmail.com>
2022-09-07 13:20:48 +0700

    Styled header

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,18 +1,44 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header class="bg-white">
-      <q-toolbar>
-        <img src="../assets/logo.png" alt="logo" width="99" height="36" />
-        <q-btn-group flat>
-          <q-btn stretch :icon="'language'" class="text-dark" />
-          <q-btn stretch><img src="../assets/rent.svg" /></q-btn>
-          <q-btn stretch :icon="'login'" class="text-dark" />
+    <q-header class="bg-white header">
+      <q-toolbar class="header__toolbar">
+        <img
+          class="header__logo"
+          src="../assets/logo.png"
+          alt="logo"
+          width="99"
+          height="36"
+        />
+        <q-btn-group flat class="header__navigation">
+          <q-btn
+            class="text-dark header__navigation-item"
+            stretch
+            :icon="'language'"
+          />
+          <q-btn class="header__navigation-item" stretch
+            ><img src="../assets/rent.svg"
+          /></q-btn>
+          <q-btn
+            class="text-dark header__navigation-item"
+            stretch
+            :icon="'login'"
+          />
         </q-btn-group>
       </q-toolbar>
     </q-header>
   </q-layout>
 </template>
 
-<style></style>
+<style lang="scss">
+.header {
+  border-bottom-right-radius: 8px;
+  border-bottom-left-radius: 8px;
+  box-shadow: 0 4px 4px rgb(0, 0, 0, 0.25);
+
+  &__navigation {
+    margin-left: auto;
+  }
+}
+</style>
 
 <script setup></script>
