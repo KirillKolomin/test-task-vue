17ca166b6f3e21488ca8f1bf20a7ec13894bf506
kirill kolomin <kirillvintorez@gmail.com>
2022-09-08 01:45:02 +0700

    Adjust styles for layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <TopHeader />
+  <q-layout view="hHh lpR fFf" class="layout">
+    <TopHeader class="layout__header" />
+    <q-page-container class="layout__page-container">
+      <q-page class="layout__page catalog">
+        <CatalogCard
+          v-for="appartment in appartments"
+          :key="appartment.id"
+          v-bind="appartment"
+          class="catalog__card"
+        />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
+
+<style scoped lang="scss">
+// Сама БЭМ методология говорит о том, что под каждый блок структура должна содержать свой собственный файл.
+// Но я пожалуй, отступлю от этого правила на время тестового задания.
+.layout {
+  &__page {
+    padding: 16px;
+  }
+}
+
+.catalog {
+  &__card {
+    & + & {
+      margin-top: 16px;
+    }
+  }
+}
+</style>
 
 <script setup>
 import TopHeader from "./components/TopHeader";
+import CatalogCard from "./components/CatalogCard";
+import appartments from "./models/apartments.json";
 </script>
--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,39 +1,39 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header class="bg-white header">
-      <q-toolbar class="header__toolbar">
-        <img
-          class="header__logo"
-          src="../assets/logo.png"
-          alt="logo"
-          width="99"
-          height="36"
+  <q-header class="bg-white header">
+    <q-toolbar class="header__toolbar">
+      <img
+        class="header__logo"
+        src="../assets/logo.png"
+        alt="logo"
+        width="99"
+        height="36"
+      />
+      <q-btn-group flat class="header__navigation">
+        <q-btn
+          class="text-dark header__navigation-item"
+          stretch
+          :icon="'language'"
         />
-        <q-btn-group flat class="header__navigation">
-          <q-btn
-            class="text-dark header__navigation-item"
-            stretch
-            :icon="'language'"
-          />
-          <q-btn class="header__navigation-item" stretch
-            ><img src="../assets/rent.svg"
-          /></q-btn>
-          <q-btn
-            class="text-dark header__navigation-item"
-            stretch
-            :icon="'login'"
-          />
-        </q-btn-group>
-      </q-toolbar>
-    </q-header>
-  </q-layout>
+        <q-btn class="header__navigation-item" stretch
+          ><img src="../assets/rent.svg"
+        /></q-btn>
+        <q-btn
+          class="text-dark header__navigation-item"
+          stretch
+          :icon="'login'"
+        />
+      </q-btn-group>
+    </q-toolbar>
+  </q-header>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
+@import "src/styles/colors";
+
 .header {
   border-bottom-right-radius: 8px;
   border-bottom-left-radius: 8px;
-  box-shadow: 0 4px 4px rgb(0, 0, 0, 0.25);
+  box-shadow: 0 4px 4px $box-shadow-color;
 
   &__navigation {
     margin-left: auto;
