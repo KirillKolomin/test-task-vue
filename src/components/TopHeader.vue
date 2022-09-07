60394a6427501f663fa36c6faf9f0269bcf4ad4a
kirill kolomin <kirillvintorez@gmail.com>
2022-09-07 12:51:43 +0700

    Add component for header

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-white">
+      <q-toolbar>
+        <img src="../assets/logo.png" alt="logo" width="99" height="36" />
+        <q-btn-group flat>
+          <q-btn stretch :icon="'language'" class="text-dark" />
+          <q-btn stretch><img src="../assets/rent.svg" /></q-btn>
+          <q-btn stretch :icon="'login'" class="text-dark" />
+        </q-btn-group>
+      </q-toolbar>
+    </q-header>
+  </q-layout>
+</template>
+
+<style></style>
+
+<script setup></script>
