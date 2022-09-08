7f7eb3c49d1677fb1b13bf3a3faa9bf4c2285fb8
kirill kolomin <kirillvintorez@gmail.com>
2022-09-08 12:48:31 +0700

    Style TopHeader component for tablet sizes

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -8,20 +8,33 @@
         width="99"
         height="36"
       />
-      <q-btn-group flat class="header__navigation">
-        <q-btn
-          class="text-dark header__navigation-item"
-          stretch
-          :icon="'language'"
-        />
-        <q-btn class="header__navigation-item" stretch
-          ><img src="../assets/rent.svg"
-        /></q-btn>
-        <q-btn
-          class="text-dark header__navigation-item"
-          stretch
-          :icon="'login'"
-        />
+
+      <q-btn-group flat class="header__navigation text-body2">
+        <q-btn class="header__navigation-item" stretch>
+          <div class="text-dark navigation-item">
+            <q-icon class="navigation-item__icon" size="16px" name="language" />
+            <span class="navigation-item__label">Язык (RU)</span>
+          </div>
+        </q-btn>
+
+        <q-btn class="header__navigation-item" stretch>
+          <div class="text-dark navigation-item">
+            <img
+              class="navigation-item__icon"
+              src="../assets/rent.svg"
+              width="16"
+              height="16"
+            />
+            <span class="navigation-item__label">Сдать жилье</span>
+          </div>
+        </q-btn>
+
+        <q-btn class="text-dark header__navigation-item" stretch>
+          <div class="text-dark navigation-item">
+            <q-icon class="navigation-item__icon" size="16px" name="login" />
+            <span class="navigation-item__label">Войти</span>
+          </div>
+        </q-btn>
       </q-btn-group>
     </q-toolbar>
   </q-header>
@@ -31,12 +44,40 @@
 @import "src/styles/colors";
 
 .header {
+  display: flex;
+  justify-content: center;
+  padding: 0 4px;
+  height: 72px;
   border-bottom-right-radius: 8px;
   border-bottom-left-radius: 8px;
   box-shadow: 0 4px 4px $box-shadow-color;
 
+  @media (min-width: $breakpoint-sm-min) {
+    height: 80px;
+  }
+
   &__navigation {
     margin-left: auto;
+  }
+
+  &__navigation-item {
+    & + & {
+      margin-left: 18px;
+    }
+  }
+}
+
+.navigation-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &__label {
+    display: none;
+
+    @media (min-width: $breakpoint-sm-min) {
+      display: initial;
+    }
   }
 }
 </style>
